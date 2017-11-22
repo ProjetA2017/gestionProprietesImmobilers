538c323e97a8f746cdab84fb636075262aa12f81
Dris AMARRAY <drisamarray@yahoo.fr>
2017-11-22 04:28:35 -0500

    Dossier dechets

--- gestionproprietes.sql
+++ gestionproprietes.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Ven 10 Novembre 2017 à 18:10
+-- Généré le :  Mer 22 Novembre 2017 à 09:19
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -45,11 +45,61 @@ CREATE TABLE `annonces` (
 --
 
 INSERT INTO `annonces` (`id`, `idannonce`, `idannonceur`, `latitude`, `longitude`, `prenom`, `nfamille`, `adresse`, `prix`, `typeannonce`, `typelogement`) VALUES
-(8, 'dris--41.1257834--73.3934342', 'dris', 41.125782, -73.393433, 'rrr', 'rrr', '331 U.S. 1, Norwalk, Connecticut, Ã‰tats-Unis', 33, '', ''),
-(12, 'dris-appartement-40.79876699999999--73.9672089', 'dris', 40.798767, -73.967209, 'sdfsdf', 'ssdfsdf', '888 Amsterdam Avenue, New York, Ã‰tat de New York, Ã‰tats-Unis', 888, 'location', 'appartement'),
-(10, 'dris-maison-40.2954808--74.68335289999999', 'dris', 40.295483, -74.683350, 'ffff', 'fff', '3333 U.S. 1, Lawrence Township, New Jersey, Ã‰tats-Unis', 3333, 'location', 'maison'),
-(9, 'dris-maison-40.853802--73.89631700000001', 'dris', 40.853802, -73.896317, 'nnnn', 'nnn', '4447 Park Avenue, Bronx, Ã‰tat de New York, Ã‰tats-Unis', 4444, 'location', 'maison'),
-(11, 'yang-maison-40.90671500000001--73.89687600000002', 'yang', 40.906715, -73.896873, 'hhh', 'hhh', '6543 Broadway, Bronx, Ã‰tat de New York, Ã‰tats-Unis', 3452, 'location', 'maison');
+(1, 'dris-maison-41.1652409--73.23439459999997', 'dris', 41.165241, -73.234398, 'kkkkkkkkkkkkkk', 'kkkkkkkkkkk', '66 U.S. 1, Fairfield, Connecticut, Ã‰tats-Unis', 555, 'location', 'maison');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annoncesapparts`
+--
+
+CREATE TABLE `annoncesapparts` (
+  `id` int(11) NOT NULL,
+  `idannonce` varchar(150) NOT NULL,
+  `typeannonce` varchar(50) NOT NULL,
+  `nbrpieces` varchar(10) DEFAULT NULL,
+  `position` varchar(15) DEFAULT NULL,
+  `animauxpermis` tinyint(1) DEFAULT NULL,
+  `inclus` varchar(50) DEFAULT NULL,
+  `infosupplementaire` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annoncesbureaux`
+--
+
+CREATE TABLE `annoncesbureaux` (
+  `id` int(11) NOT NULL,
+  `idannonce` varchar(150) NOT NULL,
+  `typeannonce` varchar(50) NOT NULL,
+  `nbremployes` varchar(10) NOT NULL,
+  `inclus` varchar(250) DEFAULT NULL,
+  `infosupplementaire` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annoncesmaison`
+--
+
+CREATE TABLE `annoncesmaison` (
+  `id` int(11) NOT NULL,
+  `idannonce` varchar(150) NOT NULL,
+  `typeannonce` varchar(50) NOT NULL,
+  `nbrchambres` varchar(10) DEFAULT NULL,
+  `inclus` varchar(150) DEFAULT NULL,
+  `infosupplementaire` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `annoncesmaison`
+--
+
+INSERT INTO `annoncesmaison` (`id`, `idannonce`, `typeannonce`, `nbrchambres`, `inclus`, `infosupplementaire`) VALUES
+(1, 'dris-maison-41.1652409--73.23439459999997', 'location', '4', 'Jardin Piscine Stationnement ', 'null');
 
 -- --------------------------------------------------------
 
@@ -70,11 +120,8 @@ CREATE TABLE `imgsannonces` (
 --
 
 INSERT INTO `imgsannonces` (`id`, `idimage`, `idannonce`, `filename`, `path`) VALUES
-(3, '11-test454.jpg', '11', 'test454.jpg', 'upload/imagesAnnonces/11-test454.jpg'),
-(5, 'yang-maison-40.90671500000001--73.89687600000002-test2292 - Copie.jpg', 'yang-maison-40.90671500000001--73.89687600000002', 'test2292 - Copie.jpg', 'upload/imagesAnnonces/yang-maison-40.90671500000001--73.89687600000002-test2292 - Copie.jpg'),
-(7, 'yang-maison-40.90671500000001--73.89687600000002-test454 - Copie.jpg', 'yang-maison-40.90671500000001--73.89687600000002', 'test454 - Copie.jpg', 'upload/imagesAnnonces/yang-maison-40.90671500000001--73.89687600000002-test454 - Copie.jpg'),
-(6, 'yang-maison-40.90671500000001--73.89687600000002-test454.jpg', 'yang-maison-40.90671500000001--73.89687600000002', 'test454.jpg', 'upload/imagesAnnonces/yang-maison-40.90671500000001--73.89687600000002-test454.jpg'),
-(4, 'yang-maison-40.90671500000001--73.89687600000002-test7dfgdfg667.jpg', 'yang-maison-40.90671500000001--73.89687600000002', 'test7dfgdfg667.jpg', 'upload/imagesAnnonces/yang-maison-40.90671500000001--73.89687600000002-test7dfgdfg667.jpg');
+(1, 'dris-maison-41.1652409--73.23439459999997-test1.jpg', 'dris-maison-41.1652409--73.23439459999997', 'test1.jpg', 'upload/imagesAnnonces/dris-maison-41.1652409--73.23439459999997-test1.jpg'),
+(2, 'dris-maison-41.1652409--73.23439459999997-test2.jpg', 'dris-maison-41.1652409--73.23439459999997', 'test2.jpg', 'upload/imagesAnnonces/dris-maison-41.1652409--73.23439459999997-test2.jpg');
 
 -- --------------------------------------------------------
 
@@ -83,7 +130,7 @@ INSERT INTO `imgsannonces` (`id`, `idimage`, `idannonce`, `filename`, `path`) VA
 --
 
 CREATE TABLE `membre` (
-  `ID_IDENTIFIANT` int(5) NOT NULL,
+  `ID_INCREMENTE` int(5) NOT NULL,
   `IDENTIFIANT` varchar(20) NOT NULL,
   `MOTDEPASSE` text NOT NULL,
   `COURRIEL` varchar(30) NOT NULL,
@@ -99,7 +146,7 @@ CREATE TABLE `membre` (
 -- Contenu de la table `membre`
 --
 
-INSERT INTO `membre` (`ID_IDENTIFIANT`, `IDENTIFIANT`, `MOTDEPASSE`, `COURRIEL`, `NOM`, `PRENOM`, `ADRESSE`, `VILLE`, `PAYS`, `TELEPHONE`) VALUES
+INSERT INTO `membre` (`ID_INCREMENTE`, `IDENTIFIANT`, `MOTDEPASSE`, `COURRIEL`, `NOM`, `PRENOM`, `ADRESSE`, `VILLE`, `PAYS`, `TELEPHONE`) VALUES
 (5, 'ali', '$2y$10$35xuFD0sCl21AeVuZlj.PuCWWwDH.tjklhbTp0WtKJUfFQ4pCfjqa', 'ali@yahoo.fr', NULL, NULL, NULL, NULL, NULL, NULL),
 (6, 'brian', '$2y$10$mufgKTkBUQOSV6fkw6rdlOSgf48GCTsVBI5UsNlexN79Os/jww7TW', 'brian@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
 (10, 'dorian', '$2y$10$QavC9wLNGQok3Zavh3ZNe.h8te2/r/weItGQOis.kDNtBQlKZCb8O', 'dorian@yahoo.fr', NULL, NULL, NULL, NULL, NULL, NULL),
@@ -107,6 +154,7 @@ INSERT INTO `membre` (`ID_IDENTIFIANT`, `IDENTIFIANT`, `MOTDEPASSE`, `COURRIEL`,
 (3, 'gilberto', '$2y$10$qKM2GxdmvMhaC4QfzoZrk..t5HcWjotEl06eUedEGlSNJDCRRP7ce', 'gilberto@yahoo.ca', NULL, NULL, NULL, NULL, NULL, NULL),
 (8, 'kjhkjh', '$2y$10$3/W38Akn5J6czuPKbTXhEOecO7DMf14TevCPlf6YnWTT/NO8/jF52', 'qqwdqwd@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
 (4, 'lucien', '$2y$10$okpR1x.9mPoYz38a1A7v6.rJAmQs62ENX3a9VACGYYEFzSi1rd0Ca', 'lucien@yahoo.fr', NULL, NULL, NULL, NULL, NULL, NULL),
+(11, 'man', '$2y$10$pGLS/Gag.LUwOuwFwXSCfOHASUhmtrTItFQ4ml4G4nh0Uk5l48Rgm', 'man@yahoo.fr', NULL, NULL, NULL, NULL, NULL, NULL),
 (9, 'paul', '$2y$10$8NzDabfUAMizADHDY3Q/4uj5RPdeADhxLonUE6FSleCybYAd.GkMe', 'paul@yahoo.fr', NULL, NULL, NULL, NULL, NULL, NULL),
 (7, 'toto', '$2y$10$z.3xGYKOaptM3l9zOywb9.tqdX02QczPNd.qW.fkCWli4XynzQS2K', 'toto@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
 (2, 'yang', '$2y$10$MTlZY2tcdqb.X7uuAxt.E.XhttiPe83FsERK8NgY7w/kr0qf1jZzu', 'yang@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL);
@@ -123,6 +171,27 @@ ALTER TABLE `annonces`
   ADD UNIQUE KEY `id` (`id`);
 
 --
+-- Index pour la table `annoncesapparts`
+--
+ALTER TABLE `annoncesapparts`
+  ADD PRIMARY KEY (`idannonce`),
+  ADD UNIQUE KEY `id_incremental` (`id`);
+
+--
+-- Index pour la table `annoncesbureaux`
+--
+ALTER TABLE `annoncesbureaux`
+  ADD PRIMARY KEY (`idannonce`),
+  ADD UNIQUE KEY `id_incremental` (`id`);
+
+--
+-- Index pour la table `annoncesmaison`
+--
+ALTER TABLE `annoncesmaison`
+  ADD PRIMARY KEY (`idannonce`),
+  ADD UNIQUE KEY `id_incremental` (`id`);
+
+--
 -- Index pour la table `imgsannonces`
 --
 ALTER TABLE `imgsannonces`
@@ -134,7 +203,7 @@ ALTER TABLE `imgsannonces`
 --
 ALTER TABLE `membre`
   ADD PRIMARY KEY (`IDENTIFIANT`),
-  ADD UNIQUE KEY `ID` (`ID_IDENTIFIANT`);
+  ADD UNIQUE KEY `ID` (`ID_INCREMENTE`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -144,17 +213,32 @@ ALTER TABLE `membre`
 -- AUTO_INCREMENT pour la table `annonces`
 --
 ALTER TABLE `annonces`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `annoncesapparts`
+--
+ALTER TABLE `annoncesapparts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `annoncesbureaux`
+--
+ALTER TABLE `annoncesbureaux`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `annoncesmaison`
+--
+ALTER TABLE `annoncesmaison`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `imgsannonces`
 --
 ALTER TABLE `imgsannonces`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `membre`
 --
 ALTER TABLE `membre`
-  MODIFY `ID_IDENTIFIANT` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `ID_INCREMENTE` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- test.js
+++
@@ -1,3 +0,0 @@
-function tetser (){
-    alert('test');
-}
